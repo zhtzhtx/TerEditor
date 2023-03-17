8f940a4674091695d1fcdfa769e8953442347cf8
zhanghanting <614274374@qq.com>
2023-03-17 15:11:21 +0800

    feat: 开发行元素

--- src/markdown-parse/node/index.js
+++ src/markdown-parse/node/index.js
@@ -1,6 +1,7 @@
 import DocumentNode from "./document-node";
 import HeadNode from "./head-node";
 import ParagraphNode from "./paragraph-node";
+import TextNode from "./text-node";
 
-export { DocumentNode, HeadNode, ParagraphNode } 
+export { DocumentNode, HeadNode, ParagraphNode, TextNode } 
 export { MNode, NodeType } from "./node";
